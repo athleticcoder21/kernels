503a3be4cbe46198ec4e3421e4203c09449e6f5b
athleticcoder21 <athleticcoder21@users.noreply.github.com>
2026-09-10 20:30:23 +0000

    deploy: 6ae5e1a71189b92ed0b3a2808a2541c7bdae2833

---
+++ assets/js/search-data.js
@@ -0,0 +1,52 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-",
+    title: "",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/kernels/";
+    },
+  },{id: "dropdown-part-0-introduction",
+              title: "Part 0. Introduction",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-part-1-making-gemv-fast",
+              title: "Part 1. Making GEMV Fast",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
---
+++ gemv/fast_kernel.cu
@@ -0,0 +1,59 @@
+// GEMV Coalesced Warp Implementation
+
+#include <cuda_runtime.h>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+
+__device__ __forceinline__ float warpReduceSum(float value) {
+    for (int offset = 16; offset > 0; offset /= 2) {
+        value += __shfl_down_sync(0xffffffff, value, offset);
+    }
+    return value;
+}
+
+__global__ void performant_gemv_kernel(
+    const float* __restrict__ A,
+    const float* __restrict__ x,
+    float* __restrict__ y,
+    int M,
+    int N    
+) {
+    // Ensure block size equals warp size for optimal performance
+    assert(blockDim.x == warpSize);
+
+    int block_id = blockIdx.x;
+    
+    if (block_id >= M)
+        return;
+
+    int thread_id = threadIdx.x;
+
+    float partial_sum = 0.f;
+
+    for (int column = thread_id; column < N; column += warpSize) {
+        partial_sum += A[block_id * N + column] * x[column];
+    }
+
+    float sum = warpReduceSum(partial_sum);
+
+    if (thread_id == 0){
+        y[block_id] = sum;
+    }
+}
+
+
+void launch_kernel(
+    const float* __restrict__ A,
+    const float* __restrict__ x,
+    float* __restrict__ y,
+    int M,
+    int N    
+) {
+    int num_threads = 32;
+
+    dim3 block_size(num_threads); 
+    dim3 grid_size(M);
+
+    performant_gemv_kernel<<<grid_size, block_size>>>(A, x, y, M, N);
+}
